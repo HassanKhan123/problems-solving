5f267589300bf2ed03c8014ede959da86fa378ae
HassanKhan123 <hk2602809@gmail.com>
2022-03-05 12:51:36 +0500

    memoized best sum problem

---
+++ bestSum.js
@@ -0,0 +1,30 @@
+const bestSum = (targetSum, elements, memo = {}) => {
+  if (targetSum in memo) return memo[targetSum];
+  if (targetSum === 0) return [];
+  if (targetSum < 0) return null;
+
+  let shortestCombination = null;
+
+  for (let num of elements) {
+    let remainder = targetSum - num;
+    let remainderCombination = bestSum(remainder, elements, memo);
+    if (remainderCombination !== null) {
+      let combination = [...remainderCombination, num];
+      if (
+        shortestCombination === null ||
+        combination.length < shortestCombination.length
+      ) {
+        shortestCombination = combination;
+      }
+    }
+  }
+
+  memo[targetSum] = shortestCombination;
+
+  return memo[targetSum];
+};
+
+console.log(bestSum(7, [5, 3, 4, 7]));
+console.log(bestSum(8, [2, 3, 5]));
+console.log(bestSum(8, [1, 4, 5]));
+console.log(bestSum(100, [1, 2, 5, 25]));
